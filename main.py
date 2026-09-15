955aff75d3ef34015c2fcebe8eaf845397828304
kadirersoy-rgb <kadir.ersoy@edu.esiee.fr>
2026-09-15 15:11:54 +0200

    Prototype map finale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-﻿"""Point d'entree Pygame : Le Labyrinthe des Ames, niveau de Kadir."""
+"""Point d'entree Pygame : Le Labyrinthe des Ames, niveau de Kadir."""
 from views.final_map_view import main
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
